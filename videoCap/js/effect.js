b9dc57e8423107e639a22765db0d549fd647dd27
aptDroid <apt.sharp@gmail.com>
2017-04-14 17:01:08 +0530

    Add files via upload

---
+++ videoCap/js/effect.js
@@ -0,0 +1,10 @@
+function showInput()
+{
+	tryButton=document.getElementById('tryButton');
+	tryButton.style.transition="5s";
+	tryButton.style.position="absolute";
+	tryButton.style.left="-400px";
+	inputSection = document.getElementById('inputSection');
+	inputSection.style.transition="2s";
+	inputSection.style.display="block";
+}
